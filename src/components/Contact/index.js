9c2405a9403341f989b36eda32b6e9d8318b35e6
obrailovska <olabrrai@icloud.com>
2022-05-05 14:05:43 -0400

    added contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+// A feature of this Hook is the ability to initialize the values of the state. In this case, we want to clear the input fields on the component loading.
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  //  This function will sync the internal state of the component formState with the user input from the DOM. The onChange event listener will ensure that the handleChange function fires whenever a keystroke is typed into the input field for name.
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            defaultValue={email}
+            name="email"
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea
+            name="message"
+            defaultValue={message}
+            onChange={handleChange}
+            rows="5"
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
